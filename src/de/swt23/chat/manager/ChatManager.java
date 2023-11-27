4906c8f541c4d082c801bf00606961caa0565364
Julian <ju.oswald@outlook.de>
2023-11-27 18:15:46 +0100

    Add manager

--- src/de/swt23/chat/manager/ChatManager.java
+++ src/de/swt23/chat/manager/ChatManager.java
@@ -1,27 +1,37 @@
 package de.swt23.chat.manager;
 
+import de.swt23.chat.message.Image;
+import de.swt23.chat.message.Message;
+import de.swt23.chat.message.Text;
+import de.swt23.chat.receiver.Group;
+import de.swt23.chat.receiver.Person;
+import de.swt23.chat.receiver.Receiver;
 import de.swt23.chat.session.Session;
 import de.thm.oop.chat.base.server.BasicTHMChatServer;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * manager class to manage connection to server and functionality
+ *
  * @author Julian Oswald
  */
 public class ChatManager {
-    private BasicTHMChatServer chatServer;
+    private final BasicTHMChatServer chatServer;
+    private final ArrayList<Group> groups;
+    private final ArrayList<Person> people;
     private Session currentSession;
-    // Arraylist for person
-    // Arraylist for groups
 
     public ChatManager() {
         chatServer = new BasicTHMChatServer();
+        groups = new ArrayList<>();
+        people = new ArrayList<>();
     }
 
     /**
      * check login data of the user
+     *
      * @param username username of the user
      * @param password password of the user
      * @return true if the login was successful
@@ -39,14 +49,24 @@ public class ChatManager {
 
     /**
      * get a list of all the users
+     *
      * @return string array of registered usernames
      */
-    public String[] getUserList() {
+    public ArrayList<Person> getUserList() {
         if (currentSession == null) {
             return null;
         }
+        // check for cached user list
+        if (!people.isEmpty()) {
+            return people;
+        }
         try {
-            return chatServer.getUsers(currentSession.getUsername(), currentSession.getPassword());
+            // get user list from server
+            String[] usernames = chatServer.getUsers(currentSession.getUsername(), currentSession.getPassword());
+            for (String username : usernames) {
+                people.add(new Person(username));
+            }
+            return people;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -54,121 +74,177 @@ public class ChatManager {
 
     /**
      * create a new group
+     *
      * @param name name of the new group
      * @return true if the group was created successfully
      */
     public boolean addGroup(String name) {
-        // add group
+        if (getGroup(name) != null) {
+            return false;
+        }
+        groups.add(new Group(name));
         return true;
     }
 
     /**
      * remove a group
+     *
      * @param name name of the group that shall be removed
      * @return true if the group was removed successfully
      */
     public boolean removeGroup(String name) {
-        // remove group
-        return true;
+        Group group = getGroup(name);
+        if (group == null) {
+            return false;
+        }
+        return groups.remove(group);
     }
 
     /**
      * get a group by its name
+     *
      * @param name the name of the searched group
      * @return the group object that matched the name, null if no object was found
      */
-    /*public Group getGroup(String name) {
+    public Group getGroup(String name) {
         for (Group group : groups) {
-            if (group.getName().equals(name)) {
+            if (group.getGroupName().equals(name)) {
                 return group;
             }
         }
         return null;
-    }*/
+    }
 
     /**
      * add a person to a group
-     * @param username username of the person that shall be added
+     *
+     * @param username  username of the person that shall be added
      * @param groupName name of the group that the person shall be added to
      * @return true if the person was added successfully
      */
     public boolean addPersonToGroup(String username, String groupName) {
-        // search user
-        // add to group
+        Person person = getPerson(username);
+        if (person == null) {
+            return false;
+        }
+        Group group = getGroup(groupName);
+        if (group == null) {
+            return false;
+        }
+        if (group.getMembers().contains(person)) {
+            return false;
+        }
+        group.getMembers().add(person);
         return true;
     }
 
     /**
      * remove a person from a group
-     * @param username username of the person that shall be removed
+     *
+     * @param username  username of the person that shall be removed
      * @param groupName name of the group that the person shall be removed from
      * @return true if the person was removed successfully
      */
     public boolean removePersonFromGroup(String username, String groupName) {
-        // search user
-        // remove from group
+        Person person = getPerson(username);
+        if (person == null) {
+            return false;
+        }
+        Group group = getGroup(groupName);
+        if (group == null) {
+            return false;
+        }
+        if (!group.getMembers().contains(person)) {
+            return false;
+        }
+        group.getMembers().add(person);
         return true;
     }
 
     /**
+     * get a person by its username
+     *
+     * @param username username of the person
+     * @return object of person or null if not found
+     */
+    public Person getPerson(String username) {
+        for (Person person : getUserList()) {
+            if (person.getUsername().equalsIgnoreCase(username)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    /**
      * send a message (text or image) to a receiver (person or group)
-     * @param message the message to be sent
+     *
+     * @param message  the message to be sent
      * @param receiver the recipient of the message
      * @return true if the message was sent successfully
      */
-    /*public boolean sendMessage(Message message, Receiver receiver) {
-        if (message instance of Image) {
+    public boolean sendMessage(Message message, Receiver receiver) {
+        if (message instanceof Image) {
             return sendImageMessage((Image) message, receiver);
         } else {
             return sendTextMessage((Text) message, receiver);
         }
-    }*/
+    }
 
     /**
      * send an image message to a receiver (group or person)
-     * @param image the image that shall be sent
+     *
+     * @param image    the image that shall be sent
      * @param receiver the receiver (can be a group or a person)
      * @return true if the message was sent successfully
      */
-    /*public boolean sendImageMessage(Image image, Receiver receiver) {
-        String mimeType = image.getMimeType();
-        InputStream imageData = image.getImageData();
-        if (receiver instanceof Group) {
-            for (Person person : (Group) receiver.getMembers()) {
-                chatServer.sendImageMessage(session.getUsername(), session.getPassword(), person.getName(), image.getMimeType(), image.getImageData());
+    public boolean sendImageMessage(Image image, Receiver receiver) {
+        try {
+            if (receiver instanceof Group) {
+                for (Person person : ((Group) receiver).getMembers()) {
+                    chatServer.sendImageMessage(currentSession.getUsername(), currentSession.getPassword(), person.getUsername(), image.getMimeType(), image.getImageData());
+                }
+            } else {
+                chatServer.sendImageMessage(currentSession.getUsername(), currentSession.getPassword(), ((Person) receiver).getUsername(), image.getMimeType(), image.getImageData());
             }
-        } else {
-            chatServer.sendTextMessage(session.getUsername(), session.getPassword(), (Person) receiver.getName(), text.getText());
+            return true;
+        } catch (IOException e) {
+            return false;
         }
-        return true;
-    }*/
+    }
 
     /**
      * send an image message to a receiver (group or person)
-     * @param text the text that shall be sent
+     *
+     * @param text     the text that shall be sent
      * @param receiver the receiver (can be a group or a person)
      * @return true if the message was sent successfully
      */
-    /*public boolean sendTextMessage(Text text, Receiver receiver) {
-        if (receiver instanceof Group) {
-            for (Person person : (Group) receiver.getMembers()) {
-                chatServer.sendTextMessage(session.getUsername(), session.getPassword(), person.getName(), text.getText());
+    public boolean sendTextMessage(Text text, Receiver receiver) {
+        try {
+            if (receiver instanceof Group) {
+                for (Person person : ((Group) receiver).getMembers()) {
+                    chatServer.sendTextMessage(currentSession.getUsername(), currentSession.getPassword(), person.getUsername(), text.getText());
+                }
+            } else {
+                chatServer.sendTextMessage(currentSession.getUsername(), currentSession.getPassword(), ((Person) receiver).getUsername(), text.getText());
             }
-        } else {
-            chatServer.sendTextMessage(session.getUsername(), session.getPassword(), (Person) receiver.getName(), text.getText());
+            return true;
+        } catch (IOException e) {
+            return false;
         }
-        return true;
-    }*/
+    }
 
     /**
      * get all existing group names
+     *
      * @return a string array containing all the group names
      */
-    /*public String[] getGroupNames() {
-        String[] groupNames = new String[groups.length];
-        for (int i = 0; i < groups.length; i++) {
-            groupNames[i] = groups[i].getName();
+    public ArrayList<String> getGroupNames() {
+        ArrayList<String> groupNames = new ArrayList<>();
+        for (Group group : groups) {
+            groupNames.add(group.getGroupName());
         }
         return groupNames;
-    }*/
+    }
 }
