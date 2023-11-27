40143a7742badd2245b1fd0b869ec8f884ba17eb
Julian <ju.oswald@outlook.de>
2023-11-27 18:16:11 +0100

    Reformat code

--- src/de/swt23/chat/receiver/Group.java
+++ src/de/swt23/chat/receiver/Group.java
@@ -1,4 +1,5 @@
 package de.swt23.chat.receiver;
+
 import java.util.ArrayList;
 
 public class Group extends Receiver {
@@ -6,23 +7,24 @@ public class Group extends Receiver {
     private final String groupName;
     private final ArrayList<Person> members;
 
-    public Group(String groupName){
+    public Group(String groupName) {
         this.groupName = groupName;
         members = new ArrayList<Person>();
     }
 
-    public String getGroupName(){
+    public String getGroupName() {
         return groupName;
     }
 
-    public void addPerson(Person member){
+    public void addPerson(Person member) {
         members.add(member);
     }
-    public boolean removePerson(Person member){
+
+    public boolean removePerson(Person member) {
         return members.remove(member);
     }
 
-    public ArrayList<Person> getMembers(){
-            return members;
-        }
+    public ArrayList<Person> getMembers() {
+        return members;
+    }
 }
