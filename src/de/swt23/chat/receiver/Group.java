e854fc4eed0258fce611e6ad351a976d132bbfee
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-11-30 12:05:01 +0100

    transferred name to Receiver

--- src/de/swt23/chat/receiver/Group.java
+++ src/de/swt23/chat/receiver/Group.java
@@ -4,16 +4,11 @@ import java.util.ArrayList;
 
 public class Group extends Receiver {
 
-    private final String groupName;
     private final ArrayList<Person> members;
 
-    public Group(String groupName) {
-        this.groupName = groupName;
+    public Group(String name) {
+        super(name);
         members = new ArrayList<Person>();
-    }
-
-    public String getGroupName() {
-        return groupName;
     }
 
     public void addPerson(Person member) {
--- src/de/swt23/chat/receiver/Person.java
+++ src/de/swt23/chat/receiver/Person.java
@@ -2,13 +2,7 @@ package de.swt23.chat.receiver;
 
 public class Person extends Receiver {
 
-    private final String username;
-
-    public Person(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
+    public Person(String name) {
+        super(name);
     }
 }
