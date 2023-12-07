a9d22341d00f5024462bf99bc81853923502cb30
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-12-07 14:16:42 +0100

    Update Hamster.java

--- src/de/swt23/chat/Hamster.java
+++ src/de/swt23/chat/Hamster.java
@@ -1,4 +1,5 @@
 package de.swt23.chat;
+
 import de.swt23.chat.message.MessageDirection;
 import de.swt23.chat.manager.ChatManager;
 import de.swt23.chat.message.Text;
@@ -7,18 +8,24 @@ import de.swt23.chat.receiver.Person;
 public class Hamster {
 
     private ChatManager manager;
+    private Person person;
+
+    public Hamster(ChatManager manager) {
+        this.manager = manager;
+        person = new Person("hamster23ws");
+    }
 
     /**
      * sends the message "v" to user "hamster32ws" for it to move forward
      */
-    public void vor(){
-        manager.sendMessage(new Text(new Person("hamster23ws"), MessageDirection.OUT, "v"));
+    public void vor() {
+        manager.sendMessage(new Text(person, MessageDirection.OUT, "v"));
     }
 
     /**
      * sends the message "l" to user "hamster32ws" for it to turn left
      */
-    public void linksUm(){
-        manager.sendMessage(new Text(new Person("hamster23ws"), MessageDirection.OUT, "l"));
+    public void linksUm() {
+        manager.sendMessage(new Text(person, MessageDirection.OUT, "l"));
     }
 }
