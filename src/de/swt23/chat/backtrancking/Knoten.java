93e4f5bf1188ec32d7e5d9a7bcee31641eb8b2de
Paul Aßmann <assmann.paul@gmx.de>
2023-12-07 14:28:49 +0100

    Olaf its your turn

---
+++ src/de/swt23/chat/backtrancking/Knoten.java
@@ -0,0 +1,19 @@
+package de.swt23.chat.backtrancking;
+
+/*class*/public class Knoten {
+    private int reihe;
+    private int spalte;
+
+    public Knoten(int reihe, int spalte) {
+        this.reihe = reihe;
+        this.spalte = spalte;
+    }
+
+    public int getReihe() {
+        return this.reihe;
+    }
+
+    public int getSpalte() {
+        return this.spalte;
+    }
+}
