0c272ef459be5dec3e3c87420873fd00d7a58380
Julian <ju.oswald@outlook.de>
2023-12-18 19:34:27 +0100

    Remove numbers (never used)

--- src/de/swt23/chat/hamster/HamsterDirection.java
+++ src/de/swt23/chat/hamster/HamsterDirection.java
@@ -1,21 +1,5 @@
 package de.swt23.chat.hamster;
 
 public enum HamsterDirection {
-
-    NORTH(0),
-
-    WEST(3),
-
-    SOUTH(2),
-
-    EAST(1);
-
-    private final int value;
-    private HamsterDirection(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    NORTH, WEST, SOUTH, EAST
 }
